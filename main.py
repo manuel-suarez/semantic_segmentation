8fd061f6c29d52053be72a1fd62e39d7d0ace3db
Arturo Suárez <manuel.suarez@cimat.mx>
2022-08-13 08:55:02 -0500

    Configuración del entorno e importación inicial de librerías

---
+++ main.py
@@ -0,0 +1,22 @@
+from glob import glob
+import shutil
+import argparse
+import zipfile
+import hashlib
+import requests
+from tqdm import tqdm
+import IPython.display as display
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import datetime, os
+from tensorflow.keras.layers import *
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.optimizers import Adam
+from IPython.display import clear_output
+import tensorflow_addons as tfa
+
+# For more information about autotune:
+# https://www.tensorflow.org/guide/data_performance#prefetching
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+print(f"Tensorflow ver. {tf.__version__}")
