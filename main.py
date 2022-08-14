3c0f7ad90a6c03d97fd3828eb068702bcf8f9f8e
Arturo Suárez <manuel.suarez@cimat.mx>
2022-08-14 09:09:32 -0500

    Apply transformations, buffer and autotune

--- main.py
+++ main.py
@@ -194,3 +194,113 @@ train_dataset = train_dataset.map(parse_image)
 
 val_dataset = tf.data.Dataset.list_files(dataset_path + val_data + "*.jpg", seed=SEED)
 val_dataset =val_dataset.map(parse_image)
+
+# Applying some transformations to our dataset
+# Here we are using the decorator @tf.function
+# if you want to know more about it:
+# https://www.tensorflow.org/api_docs/python/tf/function
+
+@tf.function
+def normalize(input_image: tf.Tensor, input_mask: tf.Tensor) -> tuple:
+    """Rescale the pixel values of the images between 0.0 and 1.0
+    compared to [0,255] originally.
+
+    Parameters
+    ----------
+    input_image : tf.Tensor
+        Tensorflow tensor containing an image of size [SIZE,SIZE,3].
+    input_mask : tf.Tensor
+        Tensorflow tensor containing an annotation of size [SIZE,SIZE,1].
+
+    Returns
+    -------
+    tuple
+        Normalized image and its annotation.
+    """
+    input_image = tf.cast(input_image, tf.float32) / 255.0
+    return input_image, input_mask
+
+@tf.function
+def load_image_train(datapoint: dict) -> tuple:
+    """Apply some transformations to an input dictionary
+    containing a train image and its annotation.
+
+    Notes
+    -----
+    An annotation is a regular  channel image.
+    If a transformation such as rotation is applied to the image,
+    the same transformation has to be applied on the annotation also.
+
+    Parameters
+    ----------
+    datapoint : dict
+        A dict containing an image and its annotation.
+
+    Returns
+    -------
+    tuple
+        A modified image and its annotation.
+    """
+    input_image = tf.image.resize(datapoint['image'], (IMG_SIZE, IMG_SIZE))
+    input_mask = tf.image.resize(datapoint['segmentation_mask'], (IMG_SIZE, IMG_SIZE))
+
+    if tf.random.uniform(()) > 0.5:
+        input_image = tf.image.flip_left_right(input_image)
+        input_mask = tf.image.flip_left_right(input_mask)
+
+    input_image, input_mask = normalize(input_image, input_mask)
+
+    return input_image, input_mask
+
+@tf.function
+def load_image_test(datapoint: dict) -> tuple:
+    """Normalize and resize a test image and its annotation.
+
+    Notes
+    -----
+    Since this is for the test set, we don't need to apply
+    any data augmentation technique.
+
+    Parameters
+    ----------
+    datapoint : dict
+        A dict containing an image and its annotation.
+
+    Returns
+    -------
+    tuple
+        A modified image and its annotation.
+    """
+    input_image = tf.image.resize(datapoint['image'], (IMG_SIZE, IMG_SIZE))
+    input_mask = tf.image.resize(datapoint['segmentation_mask'], (IMG_SIZE, IMG_SIZE))
+
+    input_image, input_mask = normalize(input_image, input_mask)
+
+    return input_image, input_mask
+
+# Buffer & autotune
+BATCH_SIZE = 5
+
+# for reference about the BUFFER_SIZE in shuffle:
+# https://stackoverflow.com/questions/46444018/meaning-of-buffer-size-in-dataset-map-dataset-prefetch-and-dataset-shuffle
+BUFFER_SIZE = 1000
+
+dataset = {"train": train_dataset, "val": val_dataset}
+
+# -- Train Dataset --#
+dataset['train'] = dataset['train'].map(load_image_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+dataset['train'] = dataset['train'].shuffle(buffer_size=BUFFER_SIZE, seed=SEED)
+dataset['train'] = dataset['train'].repeat()
+dataset['train'] = dataset['train'].batch(BATCH_SIZE)
+dataset['train'] = dataset['train'].prefetch(buffer_size=AUTOTUNE)
+
+#-- Validation Dataset --#
+dataset['val'] = dataset['val'].map(load_image_test)
+dataset['val'] = dataset['val'].repeat()
+dataset['val'] = dataset['val'].batch(BATCH_SIZE)
+dataset['val'] = dataset['val'].prefetch(buffer_size=AUTOTUNE)
+
+print(dataset['train'])
+print(dataset['val'])
+
+# how shuffle works: https://stackoverflow.com/a/53517848
